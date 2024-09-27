8b645aaa85a7ffe5514facc2e5689a40258be8ca
Nuroblox <admin@nuroblox.com>
2024-09-27 21:20:23 +0530

    Update logo

--- src/frontend/src/components/headerComponent/index.tsx
+++ src/frontend/src/components/headerComponent/index.tsx
@@ -78,7 +78,7 @@ export default function Header(): JSX.Element {
     <div className="header-arrangement relative">
       <div className="header-start-display">
         <CustomLink to="/all" className="cursor-pointer">
-          <span className="ml-4 text-2xl">NS</span>
+          <span><img src="https://nuroblox.ai/assets/images/nuro-logo.png" style="width:80px; padding:10px;"/></span>
         </CustomLink>
         {showArrowReturnIcon && (
           <Button
